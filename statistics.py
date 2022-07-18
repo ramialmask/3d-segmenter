2397053a77e27833ceff9f299429c4d9437d2b50
Rami Al-Maskari <rami.al-maskari@tum.de>
2022-07-18 15:24:40 +0200

    Added volumetric statistics

--- statistics.py
+++ statistics.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+
+def calc_volumetric_score():
+    la = lambda a, b: np.logical_and(a, b)
+    no = lambda a: np.logical_not(a)
+
+    tp = la(pred, target).sum()
+    tn = la(no(pred), no(target)).sum()
+    fp = la(pred, no(target)).sum()
+    fn = la(no(pred), target).sum()
+
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / (tp + fn)
+    vs = 1 - abs(fn - fp) / (2*tp + fp + fn)
+    accuracy = (tp + tn) / (tp + fp + tn + fn)
+    f1_dice = (2*tp) / (2*tp + fp +fn)
+    if tp == 0 and tn == 0 and fp == 0 and fn == 0:
+        f1_dice = 1
+    return precision, recall, vs, accuracy, f1_dice
+
 def calc_statistics(pred, target):
     """Calculate test metrices
     Args:
